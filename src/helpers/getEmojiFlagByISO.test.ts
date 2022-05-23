5ebfe96fc69b1202e1e789b8f4e96769016d62f6
pure-js <3690368+pure-js@users.noreply.github.com>
2022-05-23 12:10:38 +0400

    test: add first test!

---
+++ src/helpers/getEmojiFlagByISO.test.ts
@@ -0,0 +1,6 @@
+import { getEmojiFlagByISO } from './getEmojiFlagByISO';
+
+test('Getting correct emoji flag by ISO country code', () => {
+  expect(getEmojiFlagByISO('US')).toBe('🇺🇸');
+  expect(getEmojiFlagByISO('CH')).toBe('🇨🇭');
+});
