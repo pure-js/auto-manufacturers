1e7b67989e52a5ca0bd0b6dc4668ca7a580ebf69
pure-js <3690368+pure-js@users.noreply.github.com>
2022-05-26 03:30:26 +0400

    feat: add pwa config

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Auto Manufacturers
 SPA with React, TypeScript and Redux
+## Features
+Infinity scrolling
+PWA
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,27 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [
+    react(),
+    VitePWA({
+      includeAssets: ['favicon.svg'],  
+      manifest: {
+        name: 'Auto Manufacturers',
+        short_name: 'Manufacturers',
+        description: 'List of vehicle manufacturers',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'favicon.svg',
+            sizes: '512x512',
+            type: 'image/svg',
+          },
+        ]
+      }
+    })
+  ]
 });
