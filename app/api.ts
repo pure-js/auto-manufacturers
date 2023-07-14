51f210edabebeedb85e07234cd3923d6f47b0642
pure-js <3690368+pure-js@users.noreply.github.com>
2023-07-14 14:40:59 +0400

    perf: rewrite main to server side component

--- app/api.ts
+++ app/api.ts
@@ -1,7 +1,11 @@
-export const fetchManufacturers = () => {
+export async function getManufacturers(page = 1) {
   const domain = 'https://vpic.nhtsa.dot.gov';
   const endpoint = 'getallmanufacturers';
-  const args = 'format=json&page=1';
+  const args = `format=json&page=${page}`;
   const url = `${domain}/api/vehicles/${endpoint}?${args}`;
-  return fetch(url).then(data => data.json());
+  const res = await fetch(url);
+  if (!res.ok) {
+    throw new Error('Failed to fetch data')
+  }
+  return res.json();
 };
