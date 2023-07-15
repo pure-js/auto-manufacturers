e8bf813b7d6540c7819cab385decb2887d7dca58
pure-js <3690368+pure-js@users.noreply.github.com>
2023-07-15 22:31:36 +0400

    fix: use client navigation

--- app/components/Table.tsx
+++ app/components/Table.tsx
@@ -1,5 +1,6 @@
 'use client'
-import Link from 'next/link';
+ 
+import { useRouter } from 'next/navigation'
 import { ArrowTopRightOnSquareIcon } from '@heroicons/react/24/outline';
 
 import { getFlagByCountryName } from '../helpers/getEmojiFlagByISO'
@@ -12,31 +13,37 @@ interface TableProps {
   manufacturers: Array<any> | null;
 }
 
-const Table = ({ manufacturers }: TableProps) => (
-  <table className="table">
-    <thead>
-      <tr>
-        <td className='table-heading txt-right'>ID</td>
-        <td className='table-heading txt-left'>Common name</td>
-        <td className='table-heading'>Country</td>
-        <td className='table-heading'/>
-      </tr>
-    </thead>
-    <tbody>
-      { manufacturers?.map(({ Mfr_ID : Id, Country, Mfr_CommonName: Name, Mfr_Name }) => (
-        <tr key={Id} className="table-row">
-          <td className='table-cell txt-right table-cell_secondary'>{ Id }</td>
-          <td className="table-cell txt-left">{ Name ? Name : Mfr_Name }</td>
-          <td className='table-cell'>{ Country } { getFlagByCountryName(Country) }</td>
-          <td>
-            <Link href={`/manufacturers/${Id}`} className="table-cell__link">
-              <ArrowTopRightOnSquareIcon width={18} height={18} />
-            </Link>
-          </td>
-        </tr>
-      )) }
-    </tbody>
-  </table>
-);
+const Table = ({ manufacturers }: TableProps) => {
+  const router = useRouter();
 
+  return (
+    <table className="table">
+      <thead>
+        <tr>
+          <td className='table-heading txt-right'>ID</td>
+          <td className='table-heading txt-left'>Common name</td>
+          <td className='table-heading'>Country</td>
+          <td className='table-heading'/>
+        </tr>
+      </thead>
+      <tbody>
+        { manufacturers?.map(({ Mfr_ID : Id, Country, Mfr_CommonName: Name, Mfr_Name }) => (
+          <tr key={Id} className="table-row">
+            <td className='table-cell txt-right table-cell_secondary'>{ Id }</td>
+            <td className="table-cell txt-left">{ Name ? Name : Mfr_Name }</td>
+            <td className='table-cell'>{ Country } { getFlagByCountryName(Country) }</td>
+            <td>
+              <a onClick={(e) => {
+                e.preventDefault();
+                router.push(`/manufacturers/${Id}`);
+              }} href={`/manufacturers/${Id}`} className="table-cell__link">
+                <ArrowTopRightOnSquareIcon width={18} height={18} />
+              </a>
+            </td>
+          </tr>
+        )) }
+      </tbody>
+    </table>
+  );
+}
 export default Table;
