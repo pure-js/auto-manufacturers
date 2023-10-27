8d4aa991e0f522d461e7cfd3a0ddbfef9d61c092
Arthur Green <curious.dev@icloud.com>
2023-10-27 11:55:36 +0400

    chore: change imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 // import { useEffect, useState } from 'react';
 
-import Table from '../components/table';
-import { getManufacturers } from '../api/index';
+import Table from '@/components/table';
+import { getManufacturers } from '@/api/index';
 
 import styles from '@/components/layout/grid.module.css';
 
