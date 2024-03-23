f84cfa3150ce5edafaf133e2fda0a32ca88d47bb
Arthur Green <curious.dev@icloud.com>
2024-03-23 11:56:13 +0530

    style: apply prettier

--- src/helpers/getEmojiFlagByISO.test.ts
+++ src/helpers/getEmojiFlagByISO.test.ts
@@ -1,4 +1,5 @@
-import { test, expect } from 'vitest';
+import { expect, test } from 'vitest';
+
 import { getEmojiFlagByISO } from './getEmojiFlagByISO';
 
 test('Getting correct emoji flag by ISO country code', () => {
