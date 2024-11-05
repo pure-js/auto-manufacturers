496a31298cd2ab4af32cda086c58308efd074eef
Arthur Green <curious.dev@icloud.com>
2024-11-05 16:50:47 +0400

    build: up deps

--- app/manufacturers/[id]/page.tsx
+++ app/manufacturers/[id]/page.tsx
@@ -1,6 +1,7 @@
 import styles from '@/components/layout/grid.module.css';
 
-const Manufacturer = ({ params }: { params: { id: string } }) => {
+const Manufacturer = async (props: { params: Promise<{ id: string }> }) => {
+  const params = await props.params;
   return (
     <main className={styles.container}>
       <h1>{params.id}</h1>
