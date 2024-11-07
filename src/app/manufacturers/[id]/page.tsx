396caca43f63af968ad42fbdba76e1d3cb31c2a2
Arthur Green <curious.dev@icloud.com>
2024-11-07 17:17:41 +0400

    style: fix formatter

--- src/app/manufacturers/[id]/page.tsx
+++ src/app/manufacturers/[id]/page.tsx
@@ -1,14 +1,18 @@
 import styles from '@/components/layout/grid.module.css';
 
 export function generateStaticParams() {
-  return [{ id: '/manufacturers/955' }, { id: '956' }, { id: '957' }]
+  return [{ id: '/manufacturers/955' }, { id: '956' }, { id: '957' }];
 }
 
-export default async function Manufacturer ({ params }: { params: Promise<{ id: string }> }) {
-  const { id } = await params
+export default async function Manufacturer({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const { id } = await params;
   return (
     <main className={styles.container}>
       <h1>{id}</h1>
     </main>
   );
-};
+}
