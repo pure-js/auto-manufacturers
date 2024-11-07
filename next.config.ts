d66a079d34ba74acf112e17eb51514d5448f227b
Arthur Green <curious.dev@icloud.com>
2024-11-08 02:49:48 +0400

    chore: fix next config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,26 @@
+import { PHASE_DEVELOPMENT_SERVER } from 'next/constants';
+
 import type { NextConfig } from 'next';
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+export default async (phase: string) => {
+  if (phase === PHASE_DEVELOPMENT_SERVER) {
+    /**
+     * @type {import('next').NextConfig}
+     */
+    const nextConfig: NextConfig = {
+      reactStrictMode: true,
+      output: 'export',
+    };
+    return nextConfig;
+  }
 
-export default nextConfig;
+  /**
+   * @type {import('next').NextConfig}
+   */
+  const nextConfig: NextConfig = {
+    reactStrictMode: true,
+    output: 'export',
+    basePath: '/auto-manufacturers', // GitHub pages
+  };
+  return nextConfig;
+};
